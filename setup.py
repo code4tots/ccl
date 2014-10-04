006eb42e7c56682f6343d76491a6ed75f150fda9
math4tots <math4tots@gmail.com>
2014-10-03 18:51:11 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'ccl',
+    packages = ['ccl'],
+    version = '0.1',
+    description = 'Computer Command Language',
+    author = 'Kyumin Kim',
+    author_email = 'math4tots@gmail.com',
+    url = 'https://github.com/math4tots/ccl',
+    download_url = 'https://github.com/math4tots/ccl/tarball/0.1',
+    keywords = [],
+    classifiers = [],
+    install_requires= [
+        'PySide'
+    ])
