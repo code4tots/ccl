ebc0ca62c138820cefc1c10702460b4171d51764
Kyumin Kim <math4tots@users.noreply.github.com>
2014-10-06 21:34:22 -0700

    builtins 'and' and 'or'

--- ccl/lib/keywords.py
+++ ccl/lib/keywords.py
@@ -1,3 +1,27 @@
 """Implementation of macros that are traditionally Python keywords
 """
 
+from ccl.scope import register
+from ccl.ast import SpecialForm
+import ccl.exception as ex
+
+@register('and')
+@SpecialForm
+def and_(scope, args, ast):
+    if len(args) != 2:
+        raise ex.WrongNumberOfArguments(ast, expected=2, got=len(args))
+    
+    lhs, rhs = args
+    lhs = lhs(scope)
+    return rhs(scope) if lhs else lhs
+
+@register('or')
+@SpecialForm
+def or_(scope, args, ast):
+    if len(args) != 2:
+        raise ex.WrongNumberOfArguments(ast, expected=2, got=len(args))
+    
+    lhs, rhs = args
+    lhs = lhs(scope)
+    return lhs if lhs else rhs(scope)
+
