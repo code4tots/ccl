720d7a94d9a122f57444cc1a8d47e839474c831f
Kyumin Kim <math4tots@users.noreply.github.com>
2014-10-09 22:08:19 -0700

    libccl 'gui'

--- ccl/libccl/gui.py
+++ ccl/libccl/gui.py
@@ -3,11 +3,10 @@ from ccl.scope import global_scope, new_scope
 
 scope = new_scope(global_scope)
 
-run(scope = scope, string = """
+run(scope = scope, file_name = '<builtin libccl/gui.py>', string = """
 
 = tkinter $ python-import 'tkinter'
 
 """)
-
 
 global_scope['tkinter'] = scope['tkinter']
