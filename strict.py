fa83ba468a3ed974d3b54d18e3d5318adbfe080e
Kyumin Kim <math4tots@users.noreply.github.com>
2014-10-24 07:28:46 -0700

    strict.py

--- strict.py
+++ strict.py
@@ -1,12 +1,28 @@
 """A much stricter language
 
-function capacity(container : Container) : Int {
-    return size(container);
-}
+typeclass Any implements () extends ()
 
+function capacity(container : Container) : Int {
+    declare x : Int
+    declare y : Float
+    
+    x = 2 * size(container)
+    y = to_Float()
+    return size(container)
+}
 
 """
 
 from ply import lex, yacc
+
+literals = '(){}[]:;=+-*/'
+keywords = ('function','return')
+t_NAME = r'(?!\d)\w+'
+t_INT = r'(?:\+|\-)\d+(?!\.)'
+t_FLOAT = r'(?:\+|\-)\d+\.\d*'
+tokens = ('NAME', 'INT', 'FLOAT') + tuple(keyword.upper() for keyword in keywords)
+
+def p_type_name(p):
+    "type : NAME"
 
 
