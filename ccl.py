c22a1585f5f9b65b1ccab7f54f7c89810e8fefd4
Kyumin Kim <math4tots@users.noreply.github.com>
2014-10-28 09:35:40 -0700

    ccl.py

--- ccl.py
+++ ccl.py
@@ -4,64 +4,58 @@ Just make it a language I would want to use for fun.
 
 MAKE IT FUN.
 
+What if the language was regular-expression-esq?
+
+A very terse forth-like language.
+
+( p p + ) =f 1 2 $f
+
+( [ p p + ] =f ) =g
+1 2 f
+
 """
 
-from ply import lex, yacc
+def parse(string):
+    tokens = string.split()
+    stack = [ [] ]
+    for token in tokens:
+        if token in ('(', '['):
+            stack.append([])
+        elif token == ')':
+            stack[-2].append(tuple(stack.pop()))
+        elif token == ']':
+            stack[-2].append(list(stack.pop()))
+        else:
+            stack[-1].append(token)
+    if len(stack) != 1:
+        raise Exception()
+    return stack[0]
 
-keywords = {keyword : keyword.upper() for keyword in ['if', 'then', 'else']}
-symbols = {'++' : 'INCREMENT', '--' : 'DECREMENT'}
-literals = '()[]{}.,+-=/*'
+def execute(thunk, environment=None, stack=None):
+    if environment is None:
+        environment = dict()
 
-t_ignore = ' \t\n'
-t_NAME = r'(?!\d)\w+'
-t_FLOAT = r'\-?\+?\d+\.\d*'
-t_INT = r'\-?\+?\d+(?!\.)'
-t_STRING = r'\"(?:\\\"|[^"])*\"'
-t_STRING += t_STRING.replace('"',"'")
+    if stack is None:
+        stack = []
 
-tokens = ['NAME','FLOAT','INT','STRING'] + list(symbols.values()) + list(keywords.values())
-
-def p_all(p):
-    "all : expression_list"
-    def all_(scope):
-        last = None
-        for expression in p[1]:
-            last = expression(scope)
-        return last
-    p[0] = all_
-
-def p_empty_expression_list(p):
-    "expression_list : "
-    p[0] = []
-
-def p_expression_list_with_commas(p):
-    "expression_list : expression_list ',' "
-    p[0] = p[1]
-
-def p_expression_list(p):
-    "expression_list : expression_list expression"
-    p[0] = p[1]; p[0].append(p[2])
-
-def p_name(p):
-    "expression : NAME"
-    p[0] = lambda scope : scope[p[1]]
-
-def p_literal(p):
-    """expression : FLOAT
-                  | INT
-                  | STRING"""
-    p[0] = lambda scope : eval(p[1])
-
-def p_function_call(p):
-    "expression : expression '[' expression_list ']'"
-    def function_call(scope):
-        return p[1](scope)(*[e(scope) for e in p[3]])
-    p[0] = function_call
-
-lexer = lex.lex()
-parser = yacc.yacc()
-
-parser.parse("""
-print [1 2 3]
-
-""", lexer=lexer)({'print' : print})
+    if isinstance(thunk, str):
+        if thunk.startswith('='):
+            name = thunk[1:]
+            environment[name] = stack.pop()
+        elif thunk.startswith('$'):
+            name = thunk[1:]
+            stack.append(environment[name])
+        elif all(d.isdigit() or d in '+-' for d in thunk):
+            stack.append(int(thunk))
+        elif all(d.isdigit() or d in '+-.' for d in thunk):
+            stack.append(float(thunk))
+        else:
+            name = thunk[1:]
+            execute(environment[name], environment, stack)
+    elif isinstance(thunk, list):
+        stack.append(tuple(thunk))
+    elif isinstance(thunk, tuple):
+        for subthunk in thunk:
+            execute(subthunk, environment, stack)
+    else:
+        raise Exception()
