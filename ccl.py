3ae7de3d3ea2a11927ba2f3972611e44b127ca81
Kyumin Kim <math4tots@users.noreply.github.com>
2014-11-04 09:41:29 -0800

    ccl.py

--- ccl.py
+++ ccl.py
@@ -1,29 +1,121 @@
-"""
-"""
-class Context(object):
-    def __init__(self, table, stack=None, depth=0):
-        self.table = table
-        self.stack = stack or []
-        self.depth = depth
-    
-    def __getitem__(self, key):
-        return self.table[key]
+"""Fun to use and easy to implement programming language."""
+import os
 
-def execute(thunk, context):
-    if thunk == '[':
-        context.depth += 1
-        context.stack.append([])
-    elif thunk == ']':
-        context.depth -= 1
-        context.stack[-2].append(context.stack.pop())
-    elif isinstance(thunk, str) and not context.depth:
-        if thunk[0] == '$':
-            context.stack.append(context[thunk[1:]])
+def parse(string):
+    stack = [[]]
+    for token in string.split():
+        if   token == '[':
+            stack.append([])
+        elif token == ']':
+            stack[-2].append(stack.pop())
         else:
-            context[thunk](context)
-    else:
-        context.stack.append(thunk)
+            try:
+                token = int(token)
+            except ValueError:
+                try:
+                    token = float(token)
+                except ValueError:
+                    pass
+            stack[-1].append(token)
+    assert len(stack) == 1, "Missing close brackets"
+    return stack[0]
 
-def run(string, context):
-    for thunk in string.split():
-        execute(thunk, context)
+def execute(thunk, stack, scope):
+    if isinstance(thunk, (list,int,float)):
+        stack.append(thunk)
+    elif isinstance(thunk, str):
+        if thunk.startswith(':'):
+            stack.append(thunk[1:])
+        elif thunk.startswith('='):
+            scope[thunk[1:]] = stack.pop()
+        elif thunk.startswith('$'):
+            stack.append(scope[thunk[1:]])
+        else:
+            value = scope[thunk]
+            if isinstance(value, list):
+                for item in value:
+                    execute(item, stack, scope)
+            else:
+                value(stack, scope)
+    else:
+        raise Exception(thunk)
+
+def run(string, stack, scope):
+    for thunk in parse(string):
+        execute(thunk, stack, scope)
+
+scope = dict()
+
+def register(f):
+    scope[f.__name__] = f
+    return f
+
+@register
+def __print(stack, scope):
+    print(stack.pop())
+
+@register
+def __add(stack, scope):
+    stack[-2] += stack[-1]; stack.pop()
+
+@register
+def __subtract(stack, scope):
+    stack[-2] -= stack[-1]; stack.pop()
+
+@register
+def __multiply(stack, scope):
+    stack[-2] += stack[-1]; stack.pop()
+
+@register
+def __divide(stack, scope):
+    stack[-2] /= stack[-1]; stack.pop()
+
+@register
+def __singleton(stack, scope):
+    stack.append([stack.pop()])
+
+@register
+def __append(stack, scope):
+    stack[-2].append(stack[-1]); stack.pop()
+
+@register
+def __cwd(stack, scope):
+    stack.append(os.getcwd())
+
+@register
+def __ls(stack, scope):
+    stack.append(os.listdir(os.getcwd()))
+
+@register
+def __ls_(stack, scope):
+    stack.append(os.listdir(stack.pop()))
+
+@register
+def __cd(stack, scope):
+    os.chdir(stack.pop())
+
+cclrc = """
+$__print =p
+$__add =+
+$__subtract =-
+$__multiply =*
+$__divide =/
+$__singleton =.
+$__append =,
+$__cwd =cwd
+$__ls =ls
+$__ls_ =ls-
+$__cd =cd
+
+1 2 + p
+5 . 6 , 7 , p
+:hello . :world , p
+
+cwd ls- p
+ls p
+
+:.. cd cwd p ls p
+
+"""
+
+run(cclrc, [], scope)
