7b8fcc9f4df1f3f8d63983d9babaaf4dcfeb4c1a
Kyumin Kim <math4tots@users.noreply.github.com>
2014-11-15 20:58:37 -0800

    arithmetic

---
+++ ccl/lib/math.py
@@ -0,0 +1,39 @@
+from ccl.core import run
+
+def init(scope):
+    
+    @scope.register
+    def __add(stack, scope):
+        stack.append(stack.pop() + stack.pop())
+
+    @scope.register
+    def __subtract(stack, scope):
+        stack.append(stack.pop() - stack.pop())
+    
+    @scope.register
+    def __multiply(stack, scope):
+        stack.append(stack.pop() * stack.pop())
+    
+    @scope.register
+    def __divide(stack, scope):
+        stack.append(stack.pop() / stack.pop())
+    
+    @scope.register
+    def __divide(stack, scope):
+        stack.append(stack.pop() / stack.pop())
+    
+    @scope.register
+    def __floor_divide(stack, scope):
+        stack.append(stack.pop() // stack.pop())
+    
+    @scope.register
+    def __exponentiate(stack, scope):
+        stack.append(stack.pop() ** stack.pop())
+    
+    run("""
+$__add =+
+$__subtract =-
+$__multiply =*
+$__divide =/
+$__floor_divide =//
+""", [], scope)
