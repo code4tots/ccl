3e44978f3c64c6437a00f003a6908935cd5db2f2
Kyumin Kim <math4tots@users.noreply.github.com>
2014-11-27 18:15:43 -0800

    ...

--- boring.py
+++ boring.py
@@ -1,20 +1,24 @@
 """Hmm. Maybe a bit more conventional language.
+
+But with complete dependency injection.
 """
 
 from ply import lex, yacc
 
 tokens = ('NAME', 'FLOAT', 'INT', 'STRING')
-literals = '[]'
+literals = '().'
 t_ignore = ' \t\n'
 t_NAME = r'(?!\d)\w+'
 t_FLOAT = r'\d+\.\d+'
 t_INT = r'\d+(?!\.)'
 t_STRING = r'\"(?:\\\"|[^"])*\"'
+lexer = lex.lex()
+
+def p_dot_expression(p):
+  """expression : expression '.' NAME"""
+
 
 program = """
+(w.add 1 2)
 
-[
-    x = 3
-    
-]
 """
