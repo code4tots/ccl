7b7e2c10dbfd5d6500326b4e9b6ab66191b9dc78
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-02 01:58:55 -0800

    boring

--- boring.py
+++ boring.py
@@ -1,24 +1,68 @@
-"""Hmm. Maybe a bit more conventional language.
+"""S-expression grammar."""
+from collections import namedtuple
 
-But with complete dependency injection.
-"""
+SYMBOLS = ('(', ')')
+Token = namedtuple('Token', 'type value position')
+List = namedtuple('List', 'position values')
 
-from ply import lex, yacc
+def lex(string):
+  start = [0]
+  end = [0]
+  def char():          return string[end[0]:end[0]+1]
+  def last():          return string[end[0]-1:end[0]]
+  def cut():           return string[start[0]:end[0]]
+  def token(type):     return Token(type, cut(), start[0])
+  def skip(predicate):
+    while predicate(): end[0] += 1
 
-tokens = ('NAME', 'FLOAT', 'INT', 'STRING')
-literals = '().'
-t_ignore = ' \t\n'
-t_NAME = r'(?!\d)\w+'
-t_FLOAT = r'\d+\.\d+'
-t_INT = r'\d+(?!\.)'
-t_STRING = r'\"(?:\\\"|[^"])*\"'
-lexer = lex.lex()
+  while True:
+    skip(lambda: char().isspace())
+    start[0] = end[0]
+    if not char(): break
+    if char().isdigit():
+      skip(lambda: char().isdigit())
+      if char() == '.':
+        end[0] += 1
+        skip(lambda: char().isdigit())
+        yield token('FLOAT')
+      else:
+        yield token('INT')
+    elif char().isalpha() or char() == '_':
+      skip(lambda: char().isalnum() or char() == '_')
+      yield token('ID')
+    elif char() in ('"',"'"):
+      quote = char()
+      end[0] += 1
+      skip(lambda: char() != quote or last() == '\\')
+      end[0] += 1
+      yield token('STRING')
+    elif any(string.startswith(symbol, start[0]) for symbol in SYMBOLS):
+      sym = next(s for s in SYMBOLS if string.startswith(s, start[0]))
+      end[0] += len(sym)
+      yield token(sym)
+    else:
+      skip(lambda: not char().isspace())
+      raise SyntaxError(cut())
+  yield token('EOF')
 
-def p_dot_expression(p):
-  """expression : expression '.' NAME"""
+def parse(token_stream):
+  stack = [[]]
+  for token in token_stream:
+    if token.type == '(':
+      stack.append([token.position])
+    elif token.type == ')':
+      position, *values = stack.pop()
+      stack[-1].append(List(position, values))
+    else:
+      stack[-1].append(token)
+  assert len(stack) == 1, 'unclosed parenthesis'
+  return stack[0]
 
 
-program = """
-(w.add 1 2)
 
-"""
+print(parse(lex("""
+(list (1 2 3 4))
+
+(a (id 'homepage'))
+
+""")))
