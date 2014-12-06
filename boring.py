06cdd8c8e037ade503f8579e864797bafbea34ac
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-06 06:31:25 -0800

    boring.py

--- boring.py
+++ boring.py
@@ -1,87 +1,34 @@
-"""S-expression grammar."""
-from collections import namedtuple
+"""A language's core should be its AST, and not include its grammar."""
 
-SYMBOLS = ('(', ')')
-Token = namedtuple('Token', 'type value position')
-List = namedtuple('List', 'position values')
+class Node(object):
+  def __init__(self, *args, **kwargs):
+    for name, value in zip(self.attributes, args):
+      setattr(self, name, value)
+    self.init()
 
-def lex(string):
-  start = [0]
-  end = [0]
-  def char():          return string[end[0]:end[0]+1]
-  def last():          return string[end[0]-1:end[0]]
-  def cut():           return string[start[0]:end[0]]
-  def token(type):     return Token(type, cut(), start[0])
-  def skip(predicate):
-    while predicate(): end[0] += 1
+  def init(self):
+    pass
 
-  while True:
-    skip(lambda: char().isspace())
-    start[0] = end[0]
-    if not char(): break
-    if char().isdigit():
-      skip(lambda: char().isdigit())
-      if char() == '.':
-        end[0] += 1
-        skip(lambda: char().isdigit())
-        yield token('FLOAT')
-      else:
-        yield token('INT')
-    elif char().isalpha() or char() == '_':
-      skip(lambda: char().isalnum() or char() == '_')
-      yield token('ID')
-    elif char() in ('"',"'"):
-      quote = char()
-      end[0] += 1
-      skip(lambda: char() != quote or last() == '\\')
-      end[0] += 1
-      yield token('STRING')
-    elif any(string.startswith(symbol, start[0]) for symbol in SYMBOLS):
-      sym = next(s for s in SYMBOLS if string.startswith(s, start[0]))
-      end[0] += len(sym)
-      yield token(sym)
-    else:
-      skip(lambda: not char().isspace())
-      raise SyntaxError(cut())
-  yield token('EOF')
+  def execute(self, context):
+    raise NotImplemented("This node's execute method has not been implemented")
 
-def parse(token_stream):
-  stack = [[]]
-  for token in token_stream:
-    if token.type == '(':
-      stack.append([token.position])
-    elif token.type == ')':
-      position, *values = stack.pop()
-      stack[-1].append(List(position, values))
-    else:
-      stack[-1].append(token)
-  assert len(stack) == 1, 'unclosed parenthesis'
-  return stack[0]
+class FunctionCall(Node):
+  attributes = ('function', 'arguments')
 
-def evaluate(context, thunk):
-  if isinstance(thunk, List):
-    f, *args = thunk.values
-    return evaluate(context, f)(context, args)
-  elif isinstance(thunk, Token):
-    process = (float                if thunk.type == 'FLOAT'  else
-               int                  if thunk.type == 'INT'    else
-               lambda v: context[v] if thunk.type == 'ID'     else
-               eval                 if thunk.type == 'STRING' else
-               None)
-    if process is None: raise ValueError(thunk.type)
-    return process(thunk.value)
-  raise TypeError(thunk)
+class FunctionDefinition(Node):
+  attributes = ('name', 'arguments', 'body')
 
-def execute(context, thunks):
-  last = None
-  for thunk in thunks:
-    last = evaluate(context, thunk)
-  return last
+class Assignment(Node):
+  attributes = ('variable_name', 'value')
 
-print(execute(dict(), parse(lex("""
+class Literal(Node):
+  attributes = ('value',)
+  def init(self):
+    self.value = self.type(self.value)
 
-(div (id 'left-column') (body (
-  (sign-in-widget)
-  )))
+class IntegerLiteral(Literal):
+  type = int
 
-"""))))
+class FloatingLiteral(Literal):
+  type = float
+
