17cfd47564636d23f6a14ca93fdd01101b66aeef
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-16 21:13:49 -0800

    boring.py bash-like

--- boring.py
+++ boring.py
@@ -1,34 +1,78 @@
-"""A language's core should be its AST, and not include its grammar."""
+import os
+from ply import lex, yacc
 
-class Node(object):
-  def __init__(self, *args, **kwargs):
-    for name, value in zip(self.attributes, args):
-      setattr(self, name, value)
-    self.init()
+start = 'command'
+tokens = ('NUMBER', 'NAME', 'STRING', 'AND', 'OR', 'LPAR', 'RPAR', 'PIPE',
+  'SEMICOLON')
+t_ignore = ' \n\t'
+precedence = (
+  ('left', 'SEMICOLON'),
+  ('left', 'OR'),
+  ('left', 'AND'),
+  ('left', 'PIPE'),)
+t_LPAR = r'\('
+t_RPAR = r'\)'
+t_AND = r'\&\&'
+t_OR = r'\|\|'
+t_PIPE = r'\|'
+t_SEMICOLON = r'\;'
+def t_NUMBER(t): r'\d+\.?\d*'; t.value = Number(t.value); return t
+def t_NAME(t): r'(?!\d)\w+'; t.value = Name(t.value); return t
+def t_STRING(t): r'\"(?:\\\"|[^"])*\"'; t.value = String(t.value); return t
+def p_number(p): "atom : NUMBER"; p[0] = p[1]
+def p_name(p): "atom : NAME"; p[0] = p[1]
+def p_string(p): "atom : STRING"; p[0] = p[1]
+def p_parenthesis(p): "atom : LPAR command RPAR"; p[0] = p[2]
+def p_atoms0(p): "atoms : atom"; p[0] = [p[1]]
+def p_atoms1(p): "atoms : atoms atom"; p[0] = p[1]; p[0].append(p[2])
+def p_atoms(p): "command : atoms"; p[0] = Command(p[1])
+def p_pipe(p): "command : command PIPE command"; p[0] = Pipe([p[1], p[3]])
+def p_sc(p): "command : command SEMICOLON command"; p[0] = Semicolon([p[1], p[3]])
+lexer = lex.lex()
+parser = yacc.yacc()
+def parse(string): return parser.parse(string, lexer=lexer)
 
-  def init(self):
-    pass
+class Thunk(object):
+  def __repr__(self):
+    return type(self).__name__ + '(' + super(Thunk, self).__repr__() + ')'
+class Number(Thunk, float):
+  def eval(self, context): return self
+class Name(Thunk, str):
+  def eval(self, context): return context[self]
+class String(Thunk, str):
+  def eval(self, context): return eval(self)
+class Command(Thunk, list):
+  def eval(self, context, *args):
+    return self[0].eval(context)(*(args + tuple(arg.eval(context) for arg in self[1:])))
+class Pipe(Thunk, list):
+  def eval(self, context, *args):
+    lhs, rhs = self
+    return rhs.eval(context, lhs.eval(context), *args)
+class Semicolon(Thunk, list):
+  def eval(self, context, *args):
+    lhs, rhs = self
+    lhs.eval(context)
+    return rhs.eval(context, *args)
 
-  def execute(self, context):
-    raise NotImplemented("This node's execute method has not been implemented")
-
-class FunctionCall(Node):
-  attributes = ('function', 'arguments')
-
-class FunctionDefinition(Node):
-  attributes = ('name', 'arguments', 'body')
-
-class Assignment(Node):
-  attributes = ('variable_name', 'value')
-
-class Literal(Node):
-  attributes = ('value',)
-  def init(self):
-    self.value = self.type(self.value)
-
-class IntegerLiteral(Literal):
-  type = int
-
-class FloatingLiteral(Literal):
-  type = float
-
+context = dict()
+def register(name=None):
+  def inner(f): context[name or f.__name__] = f; return f
+  return inner
+@register()
+def cat(file_name):
+  with open(file_name) as f: return f.read()
+@register()
+def printf(string, *args):
+  return string % args
+@register()
+def echo(value): return value
+@register('print')
+def print_(value): print(value); return value
+@register()
+def ls(): return os.listdir()
+@register()
+def add(lhs, rhs): return lhs + rhs
+parse('''
+echo "hi" | add " there" | print ;
+print "wut"
+''').eval(context)
