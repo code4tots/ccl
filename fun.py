e7b95a1c6a41e1884f2d2eaa181cdc6bf0b89d14
Kyumin Kim <math4tots@gmail.com>
2014-12-25 01:23:05 -0800

    multiplicative_expression additive_expression

--- fun.py
+++ fun.py
@@ -1,3 +1,4 @@
+import operator
 from collections import namedtuple
 
 Token = namedtuple('Token', 'type value text mark')
@@ -43,11 +44,17 @@ class Lexer(object):
         self._mark += 1
         while self._c.isdigit():
           self._mark += 1
-      return Token(
-          type='NUMBER',
-          value=float(self._text[mark:self._mark]),
-          text=self._text,
-          mark=self._mark)
+        return Token(
+            type='FLOAT',
+            value=float(self._text[mark:self._mark]),
+            text=self._text,
+            mark=self._mark)
+      else:
+        return Token(
+            type='INT',
+            value=int(self._text[mark:self._mark]),
+            text=self._text,
+            mark=self._mark)
 
     if self._c.isalpha() or self._c == '_':
       mark = self._mark
@@ -69,7 +76,7 @@ class Lexer(object):
       self._mark += 1
       return Token(
           type='STRING',
-          value=self._text[mark:self._mark],
+          value=eval(self._text[mark:self._mark]),
           text=self._text,
           mark=self._mark)
 
@@ -91,6 +98,9 @@ class Parser(object):
     self._lexer = lexer
     self._peek = lexer.next()
 
+  def next(self):
+    pass
+
   def consume(self, construct):
     if isinstance(construct, str):
       if self._peek.type == construct:
@@ -103,7 +113,7 @@ class Parser(object):
   def expect(self, construct):
     result = self.consume(construct)
     if result is None:
-      raise SyntaxError(construct, self._peek)
+      raise SyntaxError((construct, self._peek))
     return result
 
   def token_atom(self, type_):
@@ -121,9 +131,10 @@ class Parser(object):
 
   def atom(self):
     return (
-        self.token_atom('NUMBER') or
-        self.token_atom('NAME') or
+        self.token_atom('INT') or
+        self.token_atom('FLOAT') or
         self.token_atom('STRING') or
+        self.token_atom('NAME') or
         self.parenthetical_expression())
 
   def sign_expression(self):
@@ -137,15 +148,48 @@ class Parser(object):
     else:
       return self.atom()
 
+  def multiplicative_expression(self):
+    lhs = self.sign_expression()
+    while True:
+      op = (
+          operator.mul     if self.consume('*') else
+          operator.truediv if self.consume('/') else
+          operator.mod     if self.consume('%') else
+          None)
+      if op is None: break
+      rhs = self.expect(self.sign_expression)
+      def scope(op, lhs, rhs):
+        def thunk(context):
+          return op(lhs(context), rhs(context))
+        return thunk
+      lhs = scope(op, lhs, rhs)
+    return lhs
+
+  def additive_expression(self):
+    lhs = self.multiplicative_expression()
+    while True:
+      op = (
+          operator.add     if self.consume('+') else
+          operator.sub     if self.consume('-') else
+          None)
+      if op is None: break
+      rhs = self.expect(self.multiplicative_expression)
+      def scope(op, lhs, rhs):
+        def thunk(context):
+          return op(lhs(context), rhs(context))
+        return thunk
+      lhs = scope(op, lhs, rhs)
+    return lhs
+
   def expression(self):
-    return self.sign_expression()
+    return self.additive_expression()
 
 text = """
 
-(-14.5)
+"hi " + "there"
 
 """
 
 thunk = Parser(Lexer(text)).expression()
 
-print(thunk(None))
+print(thunk(dict()))
