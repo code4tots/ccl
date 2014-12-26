8e9944a734e63f7d3e3b739e22a9ac44ad8cd062
Kyumin Kim <math4tots@gmail.com>
2014-12-25 23:16:08 -0800

    thunk.py

---
+++ ccl/thunk.py
@@ -0,0 +1,34 @@
+class Thunk(object):
+  pass
+
+class Assignable(Thunk):
+  pass
+
+class Literal(Thunk):
+  def __init__(self, value):
+    self.value = value
+
+  def __call__(self, context):
+    return self.value
+
+class Name(Assignable):
+  def __init__(self, name):
+    self.name = name
+
+  def __call__(self, context):
+    return context[self.name]
+
+  def assign(self, context, value):
+    context[self.name] = value
+    return value
+
+class FunctionCall(Thunk):
+  def __init__(self, f, args):
+    self.f = f
+    self.args = args
+
+  def __call__(self, context):
+    f = self.f(context)
+    args = [arg(context) for arg in self.args]
+    return f(*args)
+
