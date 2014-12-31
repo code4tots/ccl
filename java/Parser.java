eea1d0618731c735f88d18e3c028c9e9a7801457
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-31 10:41:11 -0800

    no private

--- java/Parser.java
+++ java/Parser.java
@@ -9,51 +9,6 @@ public class Parser {
     i = 0;
   }
 
-  public Character c() { return i < s.length() ? s.charAt(i) : null; }
-
-  public void skipSpaces() {
-    while (i < s.length() && Character.isWhitespace(s.charAt(i))) i++;
-  }
-
-  public Double number() {
-    skipSpaces();
-    if (c() == null || !Character.isDigit(c())) return null;
-    int j = i;
-    while (c() != null && (Character.isDigit(c()) || c() == '.')) i++;
-    return Double.parseDouble(s.substring(j, i));
-  }
-
-  public ArrayList<Object> stringLiteral() {
-    skipSpaces();
-    if (c() == null || c() != '"' && c() != '\'') return null;
-    int j = i;
-    char q = c();
-    i++;
-    while (c() != null && c() != q) i += (c() == '\\' ? 2 : 1);
-    i++;
-    // TODO: unescape the string.
-    return new ArrayList<Object>(java.util.Arrays.asList(new Object[]{"__literal__", s.substring(j+1, i-1)}));
-  }
-
-  public String name() {
-    skipSpaces();
-    if (c() == null || !Character.isJavaIdentifierStart(c())) return null;
-    int j = i;
-    while (c() != null && Character.isJavaIdentifierPart(c())) i++;
-    return s.substring(j, i);
-  }
-
-  public Object item() {
-    Object ret;
-    ret = number();        if (ret != null) return ret;
-    ret = stringLiteral(); if (ret != null) return ret;
-    ret = name();          if (ret != null) return ret;
-    ret = list();          if (ret != null) return ret;
-    ret = dict();          if (ret != null) return ret;
-    ret = command();       if (ret != null) return ret;
-    return null;
-  }
-
   public ArrayList<Object> items() {
     ArrayList<Object> ii = new ArrayList<Object>();
     Object it;
@@ -72,7 +27,52 @@ public class Parser {
     return new ArrayList<Object>(java.util.Arrays.asList(new Object[]{"__list__", l}));
   }
 
-  public ArrayList<Object> dict() {
+  Character c() { return i < s.length() ? s.charAt(i) : null; }
+
+  void skipSpaces() {
+    while (i < s.length() && Character.isWhitespace(s.charAt(i))) i++;
+  }
+
+  Double number() {
+    skipSpaces();
+    if (c() == null || !Character.isDigit(c())) return null;
+    int j = i;
+    while (c() != null && (Character.isDigit(c()) || c() == '.')) i++;
+    return Double.parseDouble(s.substring(j, i));
+  }
+
+  ArrayList<Object> stringLiteral() {
+    skipSpaces();
+    if (c() == null || c() != '"' && c() != '\'') return null;
+    int j = i;
+    char q = c();
+    i++;
+    while (c() != null && c() != q) i += (c() == '\\' ? 2 : 1);
+    i++;
+    // TODO: unescape the string.
+    return new ArrayList<Object>(java.util.Arrays.asList(new Object[]{"__literal__", s.substring(j+1, i-1)}));
+  }
+
+  String name() {
+    skipSpaces();
+    if (c() == null || !Character.isJavaIdentifierStart(c())) return null;
+    int j = i;
+    while (c() != null && Character.isJavaIdentifierPart(c())) i++;
+    return s.substring(j, i);
+  }
+
+  Object item() {
+    Object ret;
+    ret = number();        if (ret != null) return ret;
+    ret = stringLiteral(); if (ret != null) return ret;
+    ret = name();          if (ret != null) return ret;
+    ret = list();          if (ret != null) return ret;
+    ret = dict();          if (ret != null) return ret;
+    ret = command();       if (ret != null) return ret;
+    return null;
+  }
+
+  ArrayList<Object> dict() {
     skipSpaces();
     if (c() == null || c() != '{') return null;
     i++;
@@ -83,7 +83,7 @@ public class Parser {
     return new ArrayList<Object>(java.util.Arrays.asList(new Object[]{"__dict__", l}));
   }
 
-  public ArrayList<Object> command() {
+  ArrayList<Object> command() {
     skipSpaces();
     if (c() == null || c() != '(') return null;
     i++;
---
+++ java/README.md
@@ -0,0 +1,3 @@
+I've decided not to mark any methods private.
+
+They are purposely package private so that they can be tested.
