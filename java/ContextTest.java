4f5bce1574bde8112af1b70545ae889745232777
Kyumin Kim <math4tots@users.noreply.github.com>
2014-12-31 22:14:34 -0800

    reboot

--- java/ContextTest.java
+++
@@ -1,5 +0,0 @@
-class ContextTest extends Test {
-  public static void main(String[] args) {
-    System.out.println("All tests pass!");
-  }
-}
--- java/README.md
+++
@@ -1,3 +0,0 @@
-I've decided not to mark any methods private.
-
-They are purposely package private so that they can be tested.
