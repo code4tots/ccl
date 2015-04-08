15219c7c51d6dd6737b45cda7f12c31a8d39533c
Kyumin Kim <math4tots@gmail.com>
2015-04-08 09:02:10 -0700

    android/Example android studio app

---
+++ android/old/KyuminActivity.java
@@ -0,0 +1,13 @@
+package com.kyumin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KyuminActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // The activity is being created.
+    }
+
+}
